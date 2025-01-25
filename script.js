71ccc1d408a413dfadd565a6d9a74f32b96f867c
Daniel Rajasekar <danielrajasekar29@gmail.com>
2025-01-25 13:35:42 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+var swiper = new Swiper(".slide-content", {
+    slidesPerView: 3,
+    spaceBetween: 25,
+    
+    autoplay :{
+      delay:2500,
+      pauseOnMouseEnter: true,
+      disableOnInteraction: false
+    },
+    loop: true,
+    centerSlide: 'true',
+    fade: 'true',
+    grabCursor: 'true',
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    breakpoints:{
+        0: {
+            slidesPerView: 1,
+        },
+        520: {
+            slidesPerView: 2,
+        },
+        950: {
+            slidesPerView: 3,
+        },
+    },
+  });
